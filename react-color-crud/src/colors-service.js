6eeded1c7ecaf423aa453c3895535e1885bcff32
Danilo Strahinovic <danilo.strahinovic@cubes.rs>
2022-05-23 15:46:06 +0200

    remove old project

--- react-color-crud/src/colors-service.js
+++
@@ -1,7 +0,0 @@
-import axios from 'axios';
-
-export const getColors = () => axios.get('http://localhost:2222/api/colors/list');
-export const createColor = (name, hex_value, status) => axios.post(`http://localhost:2222/api/colors/create`, {name, hex_value, status});
-export const editColor = (id) => axios.patch(`http://localhost:2222/api/colors/${id}/edit`);
-export const changeStatus = (id) => axios.patch(`http://localhost:2222/api/colors/${id}/change-status`);
-export const deleteColor = (id) => axios.delete(`http://localhost:2222/api/colors/${id}/delete`);
